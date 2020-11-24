8862f14f5419f18444a5144a312b8dd0b6a026e9
Daniele Cottone <daniele.cottone@gmail.com>
2020-11-24 21:48:50 +0100

    log

--- app.js
+++ app.js
@@ -14,7 +14,7 @@ var roon = new RoonApi({
     website:             'https://github.com/stefan747/roon-extension-IRControl',
 
     core_paired: function(core) {
-        setup();
+        setup(); 
         update_status();
         let transport = core.services.RoonApiTransport;
         transport.subscribe_zones(function(cmd, data) {
